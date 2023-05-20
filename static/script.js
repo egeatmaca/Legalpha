d1a2dcad8464b2655a92bcbb59d3860b00dd2060
humanbeing <egeatmaca98@gmail.com>
2023-05-20 17:18:12 +0200

    representation of response texts fixed

--- static/script.js
+++ static/script.js
@@ -1,3 +1,19 @@
+function formatResponseText(responseText) {
+    if (responseText.startsWith('"')) {
+      responseText = responseText.substring(1, responseText.length);
+    }
+
+    if (responseText.endsWith('"')) {
+      responseText = responseText.substring(0, responseText.length - 1);
+    }
+
+    while (responseText.includes("\\n")) {
+      responseText = responseText.replace("\\n", "<br>");
+    }
+    
+    return responseText;
+}
+
 async function onAsk() {
     const inputText = document.getElementById("input-text");
     const input = inputText.value;
@@ -16,10 +32,11 @@ async function onAsk() {
 
     const response = await fetch("/answer?question="+input)
     const responseText = await response.text();
+    const responseTextFormatted = formatResponseText(responseText);
     
     const responseMessage = document.createElement("div");
     responseMessage.classList.add("message");
-    responseMessage.innerText = responseText;
+    responseMessage.innerHTML = responseTextFormatted;
     messages.appendChild(responseMessage);
 
     window.scrollTo(0, document.body.scrollHeight);
