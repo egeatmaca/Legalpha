58265ad1056a54645554dadff95a42a69f26b54c
egeatmaca <egeatmaca98@gmail.com>
2023-09-23 14:35:32 +0200

    ui improvements

--- static/menu.js
+++ static/menu.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
     var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
+    if (x.style.display === "flex") {
       x.style.display = "none";
     } else {
-      x.style.display = "block";
+      x.style.display = "flex";
     }
   }
